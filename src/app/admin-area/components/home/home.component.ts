3542565b0861d965f2a5f06bc98329c79299ee7b
Luong Ba Hoang <hoang2kthptlk@gmail.com>
2022-05-23 06:48:50 +0700

    update

--- src/app/admin-area/components/home/home.component.ts
+++ src/app/admin-area/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/core/services/account.service';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { TokenStorageService } from 'src/app/core/services/auth/token-storage.service';
 import { CategoryService } from 'src/app/core/services/category.service';
 import { CustomerService } from 'src/app/core/services/customer.service';
 import { InvoiceService } from 'src/app/core/services/invoice.service';
@@ -17,11 +19,16 @@ export class HomeComponent implements OnInit {
   products = []
   customers = []
   accounts = []
+
+  revenue = 0
+
   constructor(private categoryService: CategoryService,
     private invoiceService: InvoiceService,
     private productService: ProductService,
     private customerService: CustomerService,
-    private accountService: AccountService,) {
+    private accountService: AccountService,
+    public authService: AuthService,
+    public tokenStorageService: TokenStorageService) {
   }
 
   ngOnInit(): void {
@@ -36,7 +43,10 @@ export class HomeComponent implements OnInit {
       this.products = res
     })
     this.invoiceService.getAllInvoices().then(res => {
-      this.invoices = res
+      this.invoices = res.filter(i => {
+        return i.status === 4
+      })
+      this.getRevenue()
     })
     this.accountService.getAllAccounts().then(res => {
       this.accounts = res
@@ -44,6 +54,20 @@ export class HomeComponent implements OnInit {
     this.customerService.getAllCustomers().then(res => {
       this.customers = res
     })
+  }
+
+  getRevenue() {
+    let revenue = 0
+    
+    for (let j = 0; j < this.invoices.length; j++) {
+      this.productService.getProductsInInvoice(this.invoices[j].id).then(
+        res => {
+          for (let i = 0; i < res.length; i++) {
+            this.revenue += res[i].quantity * res[i].price 
+          }
+        }
+      )
+    }
   }
 
   getSuccessInvoices() {
