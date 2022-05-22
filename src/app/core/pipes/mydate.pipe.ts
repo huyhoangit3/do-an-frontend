27f49dd4a58d9d64aed641e91427d43d9b049bf5
Luong Ba Hoang <hoang2kthptlk@gmail.com>
2022-05-22 09:37:36 +0700

    update

--- src/app/core/pipes/mydate.pipe.ts
+++ src/app/core/pipes/mydate.pipe.ts
@@ -1,7 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
-import { API } from 'src/app/apiURL';
-import { environment } from 'src/environments/environment';
 
 @Pipe({
   name: 'customDate'
@@ -9,7 +7,6 @@ import { environment } from 'src/environments/environment';
 export class CustomeDate implements PipeTransform {
 
   transform(value: any, ...args: unknown[]): string {
-    const dateTime = moment(value).toISOString()
-    return moment(dateTime).utc().format('DD/MM/YYYY HH:mm:ss')
+    return moment(value).format('DD/MM/YYYY')
   }
 }
